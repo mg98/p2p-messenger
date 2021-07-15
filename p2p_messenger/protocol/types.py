68ee87c55a0edb96ac03bc9f0a204a95d3960e27
Phuc Tran Truong <ptruong@outlook.de>
2021-07-15 16:29:33 +0200

    Add description to message types

--- p2p_messenger/protocol/types.py
+++ p2p_messenger/protocol/types.py
@@ -2,10 +2,10 @@ from enum import Enum
 
 
 class MsgType(Enum):
-	PING = 0x00
-	PONG = 0x01
-	BYE = 0x02
-	JOIN = 0x03
-	QUERY = 0x10
-	QHIT = 0x11
-	POST = 0x12
+	PING = 0x00  # Discovery of the network
+	PONG = 0x01  # Response to a PING message
+	BYE = 0x02  # Departure from network
+	JOIN = 0x03  # Request to build neighbour relation, payload is sender peer id
+	QUERY = 0x10  # Search for recipient, payload is recipient peer id (which is also the public key)
+	QHIT = 0x11  # Response to query with recipient address in sender field
+	POST = 0x12  # Chat message
