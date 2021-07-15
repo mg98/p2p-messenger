7e8325329e22a8c9c22540ce9c6b0c13c0c9112e
Phuc Tran Truong <ptruong@outlook.de>
2021-07-15 15:41:22 +0200

    Add additional message types and utility functions for peer_id <-> pub_key

--- p2p_messenger/protocol/types.py
+++ p2p_messenger/protocol/types.py
@@ -4,5 +4,8 @@ from enum import Enum
 class MsgType(Enum):
 	PING = 0x00
 	PONG = 0x01
-	POST = 0x02
-	BYE = 0xFF
+	BYE = 0x02
+	JOIN = 0x03
+	QUERY = 0x10
+	QHIT = 0x11
+	POST = 0x12
--- p2p_messenger/protocol/utils.py
+++ p2p_messenger/protocol/utils.py
@@ -1,5 +1,7 @@
+import logging
 import struct
 import socket
+import rsa
 
 
 def ip_to_num(ip):
@@ -10,3 +12,27 @@ def ip_to_num(ip):
 def num_to_ip(num) -> str:
 	"""Convert number back to IP address."""
 	return socket.inet_ntoa(struct.pack('>L', num))
+
+
+def pub_key_to_peer_id(pub_key: rsa.PublicKey) -> str:
+	"""Formats public key as peer id string with 32 characters"""
+	n = str(pub_key.n)
+	e = str(pub_key.e)
+	if len(n) > 16 or len(e) > 16:
+		logging.error(f'Public key does not fit into 32 characters: n <{n}>, e <{e}>')
+	else:
+		padding = " " * (16 - len(n))
+		n += padding
+		assert len(n) <= 16
+		padding = " " * (16 - len(e))
+		e += padding
+		assert len(e) <= 16
+	return n+e
+
+
+def peer_id_to_pub_key(peer_id: str) -> rsa.PublicKey:
+	"""Convert peer id string to public key"""
+	assert len(peer_id) >= 32
+	n = int(peer_id[:16])
+	e = int(peer_id[16:32])
+	return rsa.PublicKey(n, e)
